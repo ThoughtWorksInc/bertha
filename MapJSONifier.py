85dcd7cf293a45455dabcd1ab5cd1d9080411e8f
dlockman <lockman.daniel@gmail.com>
2015-07-10 15:56:48 -0400

    Splitting of map files, adding video banner code, significant map updates

--- MapJSONifier.py
+++ MapJSONifier.py
@@ -2,23 +2,34 @@ import csv
 from geopy.geocoders import Nominatim
 import json
 
-data = csv.DictReader(open('LIST OF FELLOWS.csv'))
+data = csv.DictReader(open('FELLOW_IMPORT.csv'))
+placeholder_fellow_photo = "https://static1.squarespace.com/static/5395f905e4b052b96310e7d9/55940642e4b05111cee42a99/559e9aa7e4b0a9de8d9c7ce5/1436457657348/?format=750w&storage=local"
 
 output = {}
 
 for row in data:
-	orgLocationKey = "%s-%s" % (row['Organisation'], row['Location'])
-	if orgLocationKey in output:
-		output[orgLocationKey]['fellows'].append(row['Fellow'])
+	if row['Organisation']:
+		organization = row['Organisation']
+		orgDescription = row['Organisation Description']
+		orgLogo = row['Photo URL']
 	else:
-		output[orgLocationKey] = {
-			'location': row['Location'],
-			'organization': row['Organisation'],
-			'fellows': [row['Fellow']],
-			'description': row['Themes / Topics / Issue Area'],
+		orgLocationKey = "%s-%s" % (organization, row['Location'])
+		fellow = {
+			'name': row['Fellow'],
+			'picture': row['Photo URL'] if row['Photo URL'] else placeholder_fellow_photo,
 			'startDate': row['Start Date'],
 			'finishDate': row['Finish Date']
 		}
+		if orgLocationKey in output:
+			output[orgLocationKey]['fellows'].append(fellow)
+		else:
+			output[orgLocationKey] = {
+				'location': row['Location'],
+				'organization': organization,
+				'fellows': [fellow],
+				'description': orgDescription,
+				'logo': orgLogo
+			}
 
 geolocator = Nominatim()
 
