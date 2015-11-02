46dd0eeae1a8a9ce540b29a51f828cbbd8758fd1
Anirudh Eka <rudysback@gmail.com>
2015-11-02 12:39:34 -0500

    align video to top-left and remove webkit-appearance styling to prevent gap between left of content and left of window

--- embedBannerVideo.js
+++ embedBannerVideo.js
@@ -1,4 +1,4 @@
-var embedBannerVideo = function(videoUrl) {
+var embedBannerVideo = function(videoUrl, styleClassesToAdd) {
     $(window).scroll(function() {
       $('.bannerVideo').css({
         'top': ($(this).scrollTop()) - ($(this).scrollTop()         / 3) + "px"
@@ -9,8 +9,11 @@ var embedBannerVideo = function(videoUrl) {
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     } else {
       var banner = $('#content-wrapper #parallax-images .image-container:nth-child(1) img').first();
-      
-      $('<video class="bannerVideo" autoplay="" loop="" preload><source src="' + videoUrl + '" type="video/mp4"></video>').insertAfter(banner);
+      if(styleClassesToAdd == undefined) {
+        $('<video class="bannerVideo" autoplay="" loop="" preload><source src="' + videoUrl + '" type="video/mp4"></video>').insertAfter(banner);
+      } else {
+        $('<video class="bannerVideo '+styleClassesToAdd+'" autoplay="" loop="" preload><source src="' + videoUrl + '" type="video/mp4"></video>').insertAfter(banner);
+      }
     }
   });    
 };
